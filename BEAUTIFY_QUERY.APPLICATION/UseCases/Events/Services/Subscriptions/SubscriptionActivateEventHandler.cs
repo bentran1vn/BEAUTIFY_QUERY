45666146f08e0b4c245098b0d96f30903214e5f4
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-09 16:42:19 +0700

    Refactor subscription activation handling and update package references

--- BEAUTIFY_QUERY.APPLICATION/UseCases/Events/Services/Subscriptions/SubscriptionActivateEventHandler.cs
+++ BEAUTIFY_QUERY.APPLICATION/UseCases/Events/Services/Subscriptions/SubscriptionActivateEventHandler.cs
@@ -5,7 +5,8 @@ using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Abstractions.Repositories;
 using BEAUTIFY_QUERY.DOMAIN.Documents;
 
 namespace BEAUTIFY_QUERY.APPLICATION.UseCases.Events.Services.Subscriptions;
-internal sealed class SubscriptionActivateEventHandler : ICommandHandler<DomainEvents.SubscriptionActivated>
+internal sealed class
+    SubscriptionActivateEventHandler : ICommandHandler<DomainEvents.SubscriptionStatusActivationChanged>
 {
     private readonly IMongoRepository<SubscriptionProjection> _subscriptionRepository;
 
@@ -13,14 +14,17 @@ internal sealed class SubscriptionActivateEventHandler : ICommandHandler<DomainE
     {
         _subscriptionRepository = subscriptionRepository;
     }
-    public async Task<Result> Handle(DomainEvents.SubscriptionActivated request, CancellationToken cancellationToken)
+
+    public async Task<Result> Handle(DomainEvents.SubscriptionStatusActivationChanged request,
+        CancellationToken cancellationToken)
     {
         var subscription = await _subscriptionRepository.FindOneAsync(x => x.DocumentId == request.SubscriptionId);
         if (subscription is null)
         {
             return Result.Failure(new Error("404", "Subscription not found"));
         }
-        subscription.IsActivated = true;
+
+        subscription.IsActivated = !subscription.IsActivated;
         await _subscriptionRepository.ReplaceOneAsync(subscription);
         return Result.Success();
     }
