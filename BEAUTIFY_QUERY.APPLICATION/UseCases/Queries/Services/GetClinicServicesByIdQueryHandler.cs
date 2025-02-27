a4d642b6e64ffd449249bebc5911239524e6b962
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-27 17:43:50 +0700

    Refactor GetClinicServicesByIdQueryHandler for improved readability and formatting

--- BEAUTIFY_QUERY.APPLICATION/UseCases/Queries/Services/GetClinicServicesByIdQueryHandler.cs
+++ BEAUTIFY_QUERY.APPLICATION/UseCases/Queries/Services/GetClinicServicesByIdQueryHandler.cs
@@ -6,8 +6,9 @@ using BEAUTIFY_QUERY.DOMAIN.Documents;
 using BEAUTIFY_QUERY.DOMAIN.Entities;
 
 namespace BEAUTIFY_QUERY.APPLICATION.UseCases.Queries.Services;
-
-public class GetClinicServicesByIdQueryHandler: IQueryHandler<Query.GetClinicServicesByIdQuery, Response.GetAllServiceByIdResponse>
+public class
+    GetClinicServicesByIdQueryHandler : IQueryHandler<Query.GetClinicServicesByIdQuery,
+    Response.GetAllServiceByIdResponse>
 {
     private readonly IMongoRepository<ClinicServiceProjection> _clinicServiceRepository;
 
@@ -16,20 +17,22 @@ public class GetClinicServicesByIdQueryHandler: IQueryHandler<Query.GetClinicSer
         _clinicServiceRepository = clinicServiceRepository;
     }
 
-    public async Task<Result<Response.GetAllServiceByIdResponse>> Handle(Query.GetClinicServicesByIdQuery request, CancellationToken cancellationToken)
+    public async Task<Result<Response.GetAllServiceByIdResponse>> Handle(Query.GetClinicServicesByIdQuery request,
+        CancellationToken cancellationToken)
     {
         var isServiceExisted = await _clinicServiceRepository
             .FindOneAsync(p => p.DocumentId.Equals(request.ServiceId));
-        
-        if(isServiceExisted == null) throw new Exception($"Service {request.ServiceId} not found");
+
+        if (isServiceExisted == null) throw new Exception($"Service {request.ServiceId} not found");
 
         Response.GetAllServiceByIdResponse result;
-        
+
         if (request.MainClinicId != null)
         {
             result = new Response.GetAllServiceByIdResponse(
                 isServiceExisted.DocumentId, isServiceExisted.Name, isServiceExisted.Description,
-                isServiceExisted.MaxPrice, isServiceExisted.MinPrice, (isServiceExisted.DiscountPercent * 100).ToString(),
+                isServiceExisted.MaxPrice, isServiceExisted.MinPrice,
+                (isServiceExisted.DiscountPercent * 100).ToString(),
                 isServiceExisted.DiscountMaxPrice, isServiceExisted.DiscountMinPrice,
                 isServiceExisted.CoverImage.Select(x => new Response.Image(x.Id, x.Index, x.Url)).ToList(),
                 isServiceExisted.DescriptionImage.Select(x => new Response.Image(x.Id, x.Index, x.Url)).ToList(),
@@ -41,7 +44,7 @@ public class GetClinicServicesByIdQueryHandler: IQueryHandler<Query.GetClinicSer
                     x.Id, x.Name, x.Description, x.StepIndex, x.coverImage,
                     x.procedurePriceTypes.Select(y => new Response.ProcedurePriceType(
                         y.Id, y.Name, y.Price)).ToList()
-                )).ToList(), isServiceExisted.Promotions.Select(x => 
+                )).ToList(), isServiceExisted.Promotions.Select(x =>
                     new Response.Promotion(
                         x.Id,
                         x.Name,
@@ -50,14 +53,18 @@ public class GetClinicServicesByIdQueryHandler: IQueryHandler<Query.GetClinicSer
                         x.StartDay,
                         x.EndDate,
                         x.IsActivated
-                    )).ToList()
+                    )).ToList(), isServiceExisted.DoctorServices.Select(y =>
+                    new Response.DoctorService(y.Id, y.ServiceId,
+                        new Response.UserEntity(y.Doctor.Id, y.Doctor.FullName, y.Doctor.Email, y.Doctor.PhoneNumber,
+                            y.Doctor.ProfilePictureUrl, []))).ToList()
             );
         }
         else
         {
             result = new Response.GetAllServiceByIdResponse(
                 isServiceExisted.DocumentId, isServiceExisted.Name, isServiceExisted.Description,
-                isServiceExisted.MaxPrice, isServiceExisted.MinPrice, (isServiceExisted.DiscountPercent * 100).ToString(),
+                isServiceExisted.MaxPrice, isServiceExisted.MinPrice,
+                (isServiceExisted.DiscountPercent * 100).ToString(),
                 isServiceExisted.DiscountMaxPrice, isServiceExisted.DiscountMinPrice,
                 isServiceExisted.CoverImage.Select(x => new Response.Image(x.Id, x.Index, x.Url)).ToList(),
                 isServiceExisted.DescriptionImage.Select(x => new Response.Image(x.Id, x.Index, x.Url)).ToList(),
@@ -69,10 +76,13 @@ public class GetClinicServicesByIdQueryHandler: IQueryHandler<Query.GetClinicSer
                     x.Id, x.Name, x.Description, x.StepIndex, x.coverImage,
                     x.procedurePriceTypes.Select(y => new Response.ProcedurePriceType(
                         y.Id, y.Name, y.Price)).ToList()
-                )).ToList(), null
+                )).ToList(), null, isServiceExisted.DoctorServices.Select(y =>
+                    new Response.DoctorService(y.Id, y.ServiceId,
+                        new Response.UserEntity(y.Doctor.Id, y.Doctor.FullName, y.Doctor.Email, y.Doctor.PhoneNumber,
+                            y.Doctor.ProfilePictureUrl, []))).ToList()
             );
         }
-        
+
         return Result.Success(result);
     }
 }
