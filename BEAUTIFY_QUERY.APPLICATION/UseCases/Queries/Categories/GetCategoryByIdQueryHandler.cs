2d007a3d9dff694f2c7231494dd88d39dbdb28d0
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-03-06 18:45:29 +0700

    Refactor GetCategoryByIdQueryHandler to simplify subcategory retrieval and enhance response model

--- BEAUTIFY_QUERY.APPLICATION/UseCases/Queries/Categories/GetCategoryByIdQueryHandler.cs
+++ BEAUTIFY_QUERY.APPLICATION/UseCases/Queries/Categories/GetCategoryByIdQueryHandler.cs
@@ -1,33 +1,50 @@
 using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Abstractions.Messages;
 using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.CONTRACT.Abstractions.Shared;
 using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Abstractions.Repositories;
+using BEAUTIFY_PACKAGES.BEAUTIFY_PACKAGES.DOMAIN.Constrants;
 using BEAUTIFY_QUERY.CONTRACT.Services.Categories;
 using BEAUTIFY_QUERY.DOMAIN.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BEAUTIFY_QUERY.APPLICATION.UseCases.Queries.Categories;
-public class
-    GetCategoryByIdQueryHandler(IRepositoryBase<Category, Guid> categoryRepository)
+public class GetCategoryByIdQueryHandler(
+    IRepositoryBase<Category, Guid> categoryRepository)
     : IQueryHandler<Query.GetCategoryByIdQuery, Response.GetAllCategoriesWithSubCategories>
 {
     public async Task<Result<Response.GetAllCategoriesWithSubCategories>> Handle(Query.GetCategoryByIdQuery request,
         CancellationToken cancellationToken)
     {
-        var category = await categoryRepository.FindByIdAsync(request.Id, cancellationToken,x=>x.Children);
+        var category = await categoryRepository.FindByIdAsync(request.Id, cancellationToken, x => x.Children);
 
         if (category == null || category.IsDeleted)
         {
-            return Result.Failure<Response.GetAllCategoriesWithSubCategories>(new Error("400", "Category not found"));
+            return Result.Failure<Response.GetAllCategoriesWithSubCategories>(new Error("404", "Category not found"));
         }
 
-        var subCategories =
-            await categoryRepository.FindAll(x => x.ParentId == request.Id).ToListAsync(cancellationToken);
-        var result = new Response.GetAllCategoriesWithSubCategories(category.Id, category.Name,
+        var subCategories = category.Children?.Select(MapToResponse).ToList() ?? [];
+
+        var result = new Response.GetAllCategoriesWithSubCategories(
+            category.Id,
+            category.Name,
             category.Description ?? "",
-            category.IsParent, category.ParentId, category.IsDeleted, subCategories.Select(x =>
-                new Response.GetAllCategories(x.Id, x.Name, x.Description ?? "",
-                    x.IsParent, x.ParentId, x.IsDeleted)).ToList());
+            category.IsParent,
+            category.ParentId,
+            category.IsDeleted,
+            subCategories
+        );
 
         return Result.Success(result);
+    }
+
+    private static Response.GetAllCategories MapToResponse(Category category)
+    {
+        return new Response.GetAllCategories(
+            category.Id,
+            category.Name,
+            category.Description ?? "",
+            category.IsParent,
+            category.ParentId,
+            category.IsDeleted
+        );
     }
 }
