324731e8220da92d5205bb0c7f278917c884290b
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-04-12 16:02:21 +0700

    refactor: Remove FeedbackId property from `OrderDetail` entity
    The `FeedbackId` property in the `OrderDetail` entity has been commented out as it's no longer needed. This change simplifies the entity structure by removing an unused foreign key reference. The navigation property `Feedback` remains to maintain the relationship while avoiding redundant storage of the foreign key.

--- BEAUTIFY_QUERY.DOMAIN/Entities/OrderDetail.cs
+++ BEAUTIFY_QUERY.DOMAIN/Entities/OrderDetail.cs
@@ -8,7 +8,7 @@ public class OrderDetail : AggregateRoot<Guid>, IAuditableEntity
     public Guid ProcedurePriceTypeId { get; set; }
     public virtual ProcedurePriceType? ProcedurePriceType { get; set; }
     [Column(TypeName = "decimal(18,2)")] public decimal Price { get; set; }
-    public Guid? FeedbackId { get; set; }
+   // public Guid? FeedbackId { get; set; }
     public virtual Feedback? Feedback { get; set; }
     public DateTimeOffset CreatedOnUtc { get; set; }
     public DateTimeOffset? ModifiedOnUtc { get; set; }
