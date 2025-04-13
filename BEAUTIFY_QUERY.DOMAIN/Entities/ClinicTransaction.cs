ad2d9ad05c73bd77071ea5d209aff89c730f03d5
bentran1vn <tan182205@gmail.com>
2025-04-14 03:11:58 +0700

    Dashboard get All Fix

---
+++ BEAUTIFY_QUERY.DOMAIN/Entities/ClinicTransaction.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BEAUTIFY_QUERY.DOMAIN.Entities;
+public class ClinicTransaction : AggregateRoot<Guid>, IAuditableEntity
+{
+    public Guid? OrderId { get; set; }
+    public virtual Order? Order { get; set; }
+    public Guid? ClinicId { get; set; }
+    public virtual Clinic? Clinic { get; set; }
+    public DateTimeOffset TransactionDate { get; set; } = DateTimeOffset.UtcNow;
+    [Column(TypeName = "decimal(18,2)")] public decimal Amount { get; set; }
+    [MaxLength(50)] public string? Status { get; set; }
+    [MaxLength(50)] public string? PaymentMethod { get; set; }
+
+
+    public DateTimeOffset CreatedOnUtc { get; set; }
+    public DateTimeOffset? ModifiedOnUtc { get; set; }
+}
