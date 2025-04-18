73e1ec217a337e1d9f8762611d02702faee32e97
bentran1vn <tan182205@gmail.com>
2025-04-18 19:02:43 +0700

    GetDetailReApply

--- BEAUTIFY_QUERY.CONTRACT/Services/Clinics/Response.cs
+++ BEAUTIFY_QUERY.CONTRACT/Services/Clinics/Response.cs
@@ -83,6 +83,25 @@ public static class Response
         Subscription? currentSubscription = null,
         PagedResult<GetClinicDetail>? Branches = null,
         List<Services.Response.GetAllServiceInGetClinicById>? Services = null);
+    
+    public class MyClinicApply
+    {
+        public string Name {get; set;}
+        public string Email {get; set;}
+        public string PhoneNumber {get; set;}
+        public string City {get; set;}
+        public string District {get; set;}
+        public string Ward {get; set;}
+        public string Address {get; set;}
+        public string TaxCode {get; set;}
+        public string BankName {get; set;}
+        public string BankAccountNumber {get; set;}
+        public string BusinessLicense {get; set;}
+        public string OperatingLicense {get; set;}
+        public DateTimeOffset OperatingLicenseExpiryDate {get; set;}
+        public string ProfilePictureUrl {get; set;}
+        public string RejectReason {get; set;}
+    }
 
     public record Subscription(
         Guid Id,
