d3b3523d25f2d6726293bc56e610fb6a9c7fe778
bentran1vn <tan182205@gmail.com>
2025-04-23 15:48:26 +0700

    Get doctor by service

--- BEAUTIFY_QUERY.APPLICATION/UseCases/Queries/Services/GetDoctorClinicServicesByIdQuery.cs
+++ BEAUTIFY_QUERY.APPLICATION/UseCases/Queries/Services/GetDoctorClinicServicesByIdQuery.cs
@@ -1,49 +1,63 @@
 using BEAUTIFY_QUERY.CONTRACT.Services.Services;
 using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver.Linq;
+using Clinic = BEAUTIFY_QUERY.DOMAIN.Entities.Clinic;
 
 namespace BEAUTIFY_QUERY.APPLICATION.UseCases.Queries.Services;
 
 public class GetDoctorClinicServicesByIdQuery(
-    IMongoRepository<ClinicServiceProjection> clinicServiceRepository,
-    IRepositoryBase<DoctorCertificate, Guid> doctorCertificateRepository)
-    : IQueryHandler<Query.GetDoctorClinicServicesByIdQuery, Response.GetAllDoctorServiceByIdResponse>
+    IRepositoryBase<DoctorService, Guid> doctorServiceRepository,
+    IRepositoryBase<Clinic, Guid> clinicRepository)
+    : IQueryHandler<Query.GetDoctorClinicServicesByIdQuery, PagedResult<Response.GetAllDoctorServiceByIdResponse>>
 {
     
-    public async Task<Result<Response.GetAllDoctorServiceByIdResponse>> Handle(Query.GetDoctorClinicServicesByIdQuery request, CancellationToken cancellationToken)
+    public async Task<Result<PagedResult<Response.GetAllDoctorServiceByIdResponse>>> Handle(Query.GetDoctorClinicServicesByIdQuery request, CancellationToken cancellationToken)
     {
-        var isServiceExisted = await clinicServiceRepository.FindOneAsync(p => p.DocumentId.Equals(request.ServiceId));
-
+        var isServiceExisted = await clinicRepository.FindByIdAsync(request.ServiceId, cancellationToken);
+        
         if (isServiceExisted == null)
             throw new Exception($"Service {request.ServiceId} not found");
-        
-        var listDoctorId = isServiceExisted.DoctorServices.Select(x => x.Doctor.Id).ToList();
-        var doctorCertificates = await doctorCertificateRepository
-            .FindAll(x => listDoctorId.Contains(x.DoctorId))
-            .ToListAsync(cancellationToken);
 
-        Response.GetAllDoctorServiceByIdResponse result;
+        var query = doctorServiceRepository.FindAll(p =>
+                p.ServiceId.Equals(request.ServiceId) && !p.IsDeleted)
+            .Include(x => x.Doctor)
+            .ThenInclude(x => x.DoctorCertificates)
+            .Include(x => x.Doctor)
+            .ThenInclude(x => x.UserClinics)
+            .ThenInclude(x => x.Clinic);
 
-        result = new Response.GetAllDoctorServiceByIdResponse(
-            isServiceExisted.DocumentId,
-            isServiceExisted.DoctorServices.Select(y => new Response.DoctorService(
-                y.Id,
-                y.ServiceId,
-                new Response.UserEntity(
-                    y.Doctor.Id,
-                    y.Doctor.FullName,
-                    y.Doctor.Email,
-                    y.Doctor.PhoneNumber,
-                    y.Doctor.ProfilePictureUrl,
-                    doctorCertificates
-                        .Where(c => c.DoctorId == y.Doctor.Id)
-                        .Select(c => new Response.CertificateEntity
-                        {
-                            Id = c.Id,
-                            CertificateUrl = c.CertificateUrl,
-                            CertificateName = c.CertificateName,
-                            ExpiryDate = c.ExpiryDate,
-                            Note = c.Note
-                        }).ToList()))).ToList()
+        var doctors = await PagedResult<DoctorService>.CreateAsync(query, request.PageNumber, request.PageSize);
+
+        PagedResult<Response.GetAllDoctorServiceByIdResponse> result = PagedResult<Response.GetAllDoctorServiceByIdResponse>.Create(
+            doctors.Items.Select(x =>
+            {
+                var clinic =  x.Doctor.UserClinics.Where(x => x.ClinicId == x.Clinic.Id).SingleOrDefault();
+                return new Response.GetAllDoctorServiceByIdResponse(
+                    x.Id,
+                    new Response.UserEntity(
+                        x.Doctor.Id, x.Doctor.FullName, x.Doctor.Email, x.Doctor.PhoneNumber,
+                        x.Doctor.ProfilePicture, x.Doctor.DoctorCertificates.Select(x =>
+                            new Response.CertificateEntity()
+                            {
+                                Id = x.Id,
+                                CertificateUrl = x.CertificateUrl,
+                                CertificateName = x.CertificateName,
+                                ExpiryDate = x.ExpiryDate,
+                                Note = x.Note
+                            }).ToList()
+                    ),
+                    new Response.Clinic(
+                        clinic.Clinic.Id, clinic.Clinic.Name, clinic.Clinic.Email, clinic.Clinic.Address,
+                        clinic.Clinic.PhoneNumber, (TimeSpan)clinic.Clinic.WorkingTimeStart,
+                        (TimeSpan)clinic.Clinic.WorkingTimeEnd,
+                        clinic.Clinic.ProfilePictureUrl, clinic.Clinic.IsParent, clinic.Clinic.IsActivated,
+                        clinic.Clinic.ParentId
+                    )
+                );
+            }).ToList(),
+            doctors.PageIndex,
+            doctors.PageSize,
+            doctors.TotalCount
         );
 
         return Result.Success(result);
